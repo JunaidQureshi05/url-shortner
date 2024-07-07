e071ed2acabb9903aad9fab322401d01b322a1e4
Juned Ahmad <juned.amhad@myparkplus.com>
2024-07-07 11:27:07 +0530

    setup router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,40 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import AppLayout from "./components/layouts/AppLayout/AppLayout";
+import Landing from "./pages/Landing/Landing";
+import Dashboard from "./pages/Dashboard/Dashboard";
+import Auth from "./pages/Auth/Auth";
+import Link from "./pages/Link/Link";
+import RedirectionLink from "./pages/RedirectionLink/RedirectionLink";
+
 function App() {
-  return <div className="text-2xl">Junaid </div>;
+  const router = createBrowserRouter([
+    {
+      element: <AppLayout />,
+      children: [
+        {
+          path: "/",
+          element: <Landing />,
+        },
+        {
+          path: "/dashboard",
+          element: <Dashboard />,
+        },
+        {
+          path: "/auth",
+          element: <Auth />,
+        },
+        {
+          path: "/link/:link",
+          element: <Link />,
+        },
+        {
+          path: "/:id",
+          element: <RedirectionLink />,
+        },
+      ],
+    },
+  ]);
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ src/components/layouts/AppLayout/AppLayout.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+
+const AppLayout = () => {
+  return (
+    <div>
+      <main>
+        {/* Header */}
+
+        {/* Body */}
+        <Outlet />
+      </main>
+      {/* Footer */}
+    </div>
+  );
+};
+
+export default AppLayout;
