05467702f8fca3a72b68db91dcc470422786e76b
Juned Ahmad <juned.amhad@myparkplus.com>
2024-07-08 12:59:02 +0530

    add signup api

--- src/db/apiAuth.js
+++ src/db/apiAuth.js
@@ -1,6 +1,4 @@
-import supabase from "./supabase";
-
-console.log("@@@@@@@", supabase);
+import supabase, { supabaseUrl } from "./supabase";
 
 export async function login({ email, password }) {
   debugger;
@@ -20,4 +18,22 @@ export async function getCurrentUser() {
   if (!session.session) return null;
   if (error) throw new Error(error.message);
   return session.session;
+}
+
+export async function signup({ name, email, password, profile_pic }) {
+  const fileName = `dp-${name.split(" ").join("-")}-${Math.random()}`;
+  const { error: storageError } = await supabase.storage
+    .from("profile_pic")
+    .upload(fileName, profile_pic);
+  if (storageError) throw new Error(storageError.message);
+  const { data, error } = await supabase.auth.signUp({
+    email,
+    password,
+    options: {
+      data: {
+        name,
+        profile_pic: `${supabaseUrl}/storage/v1/object/public/profile_pic/${fileName}`,
+      },
+    },
+  });
 }
